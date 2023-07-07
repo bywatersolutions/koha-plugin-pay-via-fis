b6da18e2417c495c949453613e8566eeceafd6e1
Kyle M Hall <kyle@bywatersolutions.com>
2023-07-07 07:10:50 -0400

    Update API.pm

--- Koha/Plugin/Com/ByWaterSolutions/PayViaFIS/API.pm
+++ Koha/Plugin/Com/ByWaterSolutions/PayViaFIS/API.pm
@@ -6,16 +6,26 @@ use Mojo::Base 'Mojolicious::Controller';
 
 use WWW::Form::UrlEncoded qw(parse_urlencoded);
 
-our $ENABLE_DEBUGGING = 0;
+our $ENABLE_DEBUGGING = 1;
 
 sub handle_payment {
+    warn "Koha::Plugin::Com::ByWaterSolutions::PayViaFIS::API::handle_payment" if $ENABLE_DEBUGGING;
     my $c = shift->openapi->valid_input or return;
 
     my $params = $c->req->params->to_hash;
-    warn "PARAMS: " . Data::Dumper::Dumper($params) if $ENABLE_DEBUGGING;
+    warn "FIS: PARAMS: " . Data::Dumper::Dumper($params) if $ENABLE_DEBUGGING;
 
-    my $transaction_id = $params->{TransactionId};
-    warn "FIS: TRANSACTION ID: $transaction_id";
+    my $transaction_id = $params->{FISTransactionNumber};
+    my $borrowernumber = $params->{UserPart3} || $params->{BorrowerNumber};
+    my $MerchantCode = $params->{MerchantCode};
+    my $SettleCode = $params->{SettleCode};
+
+    my $li = $params->{LineItems};
+    $li =~ s/^\s+|\s+$//g;
+    my @line_items = split( /\,?\[(.*)\]\n?/, $li );
+    @line_items = grep { $_ ne q{} } @line_items;
+
+    warn "FIS: TRANSACTION ID: $transaction_id" if $ENABLE_DEBUGGING;
 
     my $merchant_code =
       C4::Context->preference('FisMerchantCode');    #33WSH-LIBRA-PDWEB-W
@@ -23,45 +33,17 @@ sub handle_payment {
       C4::Context->preference('FisSettleCode');      #33WSH-LIBRA-PDWEB-00
     my $password = C4::Context->preference('FisApiPassword');    #testpass;
 
-    my $ua  = LWP::UserAgent->new;
-    my $url = C4::Context->preference('FisApiUrl')
-      ;    #https://paydirectapi.ca.link2gov.com/ProcessTransactionStatus;
-    my $response = $ua->post(
-        $url,
-        {
-            L2GMerchantCode       => $merchant_code,
-            Password              => $password,
-            SettleMerchantCode    => $settle_code,
-            OriginalTransactionId => $transaction_id,
-        }
-    );
+    return $c->render(
+        status  => 500,
+        openapi => { error => 'invalid_merchant_code' }
+    ) unless ( $merchant_code eq $MerchantCode );
 
     return $c->render(
         status  => 500,
-        openapi => { error => $$response->status_line }
-    ) unless $response->is_success;
+        openapi => { error => 'invalid_settle_code' }
+    ) unless ( $settle_code eq $SettleCode );
 
-    my ( $m, $v );
-
-    warn "FIS: RESPONSE CONTENT - ***" . $response->decoded_content . "***"
-      if $ENABLE_DEBUGGING;
-
-    my @params = split( '&', uri_unescape( $response->decoded_content ) );
-    foreach my $p (@params) {
-        my ( $key, $value ) = split( '=', $p );
-        $params->{$key} = $value // q{};
-    }
-
-    warn "FIS: INCOMING PARAMS - " . Data::Dumper::Dumper($params)
-      if $ENABLE_DEBUGGING;
-
-    return $c->render(
-        status  => 500,
-        openapi => { error => 'invalid_payment' }
-    ) unless $params->{TransactionID} eq $transaction_id;
-
-    my $borrowernumber = $params->{UserPart3};
-    my $note           = "FIS: $transaction_id";
+    my $note = "FIS: $transaction_id";
 
     return $c->render(
         status  => 500,
@@ -70,17 +52,16 @@ sub handle_payment {
       if Koha::Account::Lines->search(
         { borrowernumber => $borrowernumber, note => $note } )->count();
 
-    my @line_items = split( /,/, $params->{LineItems} );
     warn "FIS: LINE ITEMS - " . Data::Dumper::Dumper( \@line_items )
       if $ENABLE_DEBUGGING;
 
     my @paid;
+    warn "FIS: BORROWERNUMBER - $borrowernumber" if $ENABLE_DEBUGGING;
     my $account = Koha::Account->new( { patron_id => $borrowernumber } );
     foreach my $l (@line_items) {
         warn "FIS: LINE ITEM - ***$l***" if $ENABLE_DEBUGGING;
-        $l = substr( $l, 1, length($l) - 2 );
-        my ( undef, $id, $description, $amount ) =
-          split( /[\*,\~]/, $l );
+        $l =~ s/^\s+|\s+$//g;
+        my ( $i, $id, $description, $amount ) = split( /\*/, $l );
 
         warn "FIS: ACCOUNTLINE TO PAY ID - $id" if $ENABLE_DEBUGGING;
         warn "FIS: DESC - $description"         if $ENABLE_DEBUGGING;
@@ -94,6 +75,8 @@ sub handle_payment {
                 amount          => $amount
             }
         );
+
+
 
         $account->pay(
             {
